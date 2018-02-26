d95eaebf449086915d4ec3cbc6e8f089351030ac
Isaac Tay <Isaac@makerforce.io>
2018-02-26 17:46:01 +0800

    Line follow logic all fixed up

--- src/line_follow.py
+++ src/line_follow.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python
 
+import numpy as np
 import cv2 as cv
 
 c_width = 1/20
 
 def line_follow(frame): #Returns (angle, lean)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    mask = cv.threshold(gray, 30, 255, cv.THRESH_BINARY_INV)[1]
-    mask = cv.GaussianBlur(mask, (5,5), 0)
+    mask = cv.inRange(frame, np.array([0, 0, 0]), np.array([100, 100, 100]))
+    mask = cv.blur(mask, (10,10))
+    mask = cv.threshold(mask, 100, 255, cv.THRESH_BINARY)[1]
     
     contours = cv.findContours(mask, 1, cv.CHAIN_APPROX_NONE)[1]
-    
+
     if len(contours) > 0:
         cnt = max(contours, key=cv.contourArea)
         
         rect = cv.minAreaRect(cnt)
         angle = rect[2]
-
-        M = cv.moments(cnt)
-
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
-
-        cv.line(frame,(cx,0),(cx,frame.shape[0]),(255,0,0),3)
+        if rect[1][0] > rect[1][1]:
+            angle = 90 + angle
         
+        if (cv.contourArea(cnt) < 0.01 * (frame.shape[0] * frame.shape[1])):
+            return None
         cv.drawContours(frame, [cnt], 0, (0,255,0), 1)
+        box = cv.boxPoints(rect)
+        box = np.int0(box)
+        cv.drawContours(frame,[box],0,(0,0,255),2)
+        if angle < -30 or angle > 30:
+            return frame, (angle, None)
 
-        lean = 0
-        if not (10 > angle > 10):
-            lean = cx - frame.shape[0]/2
-
-        return frame, (angle, lean)
-        
+        cx = int(rect[0][0])
+        cv.line(frame,(cx,0),(cx,frame.shape[0]),(255,0,0),3)
+        return frame, (angle, rect[0][0] - frame.shape[1]/2)
     else:
         return None
